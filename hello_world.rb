ec0726cbc78bf5411e130121931fc8907c989733
Genie Guo <genie@intventures.co>
2018-10-10 17:36:00 -0700

    added routes

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,29 @@
+# hello_world.rb
+
+require_relative 'advice' # loads advice.rb
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    case env['REQUEST_PATH']
+    when '/'
+      [
+        '200', 
+        {'Content-Type' => 'text/html'}, 
+        ["<html><body><h2>Hello World!</h2></body></html>"]
+      ]
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      [
+        '200', 
+        {'Content-Type' => 'text/html'}, 
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
+    else
+      [
+        '404',
+        {'Content-Type' => 'text/html', 'Content-Length' => '48'},
+        ["<html><body><h4>404 Not Found</h4></body></html>"]
+      ]
+    end
   end
 end
